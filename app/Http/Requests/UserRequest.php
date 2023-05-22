427802a345d741ff7c2d3b4b240c5b0c743027c0
kelvinhenri99 <kelvin5henri@gmail.com>
2023-05-22 17:40:39 -0300

    feat: creating the user resource > Design Pattern

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class UserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'min:3',
+                'max:255'
+            ],
+        ];
+        if ($this->method() === 'PUT') {
+            $rules['email'] = [
+                'required',
+                'email',
+                'max:255',
+                "unique:users,email,{$this->id}"
+            ];
+
+            $rules['password'] = [
+                'nullable',
+                'min:6',
+                'max:100',
+            ];
+        }
+    }
+}
